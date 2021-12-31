457f3e42c943c47c734fc6703ecfe78d3d3a7d68
cemtanrikut <cemtanrikut@outlook.com>
2021-12-31 22:22:56 +0300

    Add Post struct

--- api/post/post.go
+++ api/post/post.go
@@ -1,1 +1,15 @@
 package post
+
+import "time"
+
+type Post struct {
+	Author      string    `json:"author"`
+	AuthorImage string    `json:"author_image"`
+	Title       string    `json:"title"`
+	Content     string    `json:"content"`
+	Image       string    `json:"image"`
+	CreateDate  time.Time `json:"create_date"`
+	UpdateDate  time.Time `json:"update_date"`
+	IsActive    bool      `json:"is_active"`
+	Tag         string    `json:"tag"`
+}
